54de5bc7874f147f3c9d5a759a2bdac4ac565128
Tomoya <200378857@student.georgianc.on.ca>
2020-03-25 18:26:52 -0400

    Add animation

--- Places/settings.py
+++ Places/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tomokuro.pythonanywhere.com']
+ALLOWED_HOSTS = ['tomokuro.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,47 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let controller = new ScrollMagic.Controller();
+  // let controller = new ScrollMagic.Controller();
 
   let timeline = new TimelineMax();
   timeline
-    .to(
-      "#second",
-      6,
+    .from(".title h1", 1, {
+      y: 100,
+      opacity: 0,
+      delay: 0.2
+    })
+    .from(
+      ".title h5",
+      1,
       {
-        y: -200
+        y: 30,
+        opacity: 0
       },
-      "-=6"
-    )
-    .to(
-      "#first",
-      6,
-      {
-        y: -100
-      },
-      "-=6"
-    )
-    .to(
-      ".content, .blur",
-      6,
-      {
-        top: "0%"
-      },
-      "-=6"
-    )
-    .to(
-      ".title, nav, ",
-      6,
-      {
-        y: -600
-      },
-      "-=6"
+      "-=.5"
     );
+  // .to(
+  //   "#first",
+  //   6,
+  //   {
+  //     y: -100
+  //   },
+  //   "-=6"
+  // )
+  // .to(
+  //   ".content, .blur",
+  //   6,
+  //   {
+  //     top: "0%"
+  //   },
+  //   "-=6"
+  // )
+  // .to(
+  //   ".title, nav, ",
+  //   6,
+  //   {
+  //     y: -600
+  //   },
+  //   "-=6"
+  // );
 
-  let scene = new ScrollMagic.Scene({
-    triggerElement: "section",
-    duration: "200%",
-    triggerHook: 0
-  })
-    .setTween(timeline)
-    .setPin("section")
-    .addTo(controller);
+  // let scene = new ScrollMagic.Scene({
+  //   triggerElement: "section",
+  //   duration: "200%",
+  //   triggerHook: 0
+  // })
+  //   .setTween(timeline)
+  //   .setPin("section")
+  //   .addTo(controller);
+
+  AOS.init();
 });
